f679a014f4969e94c4b01dbeacf6f1305fae3f30
Josh Walawender <jmwalawender@gmail.com>
2022-09-07 16:24:31 -1000

    fix

--- kpf/fvc/__init__.py
+++ kpf/fvc/__init__.py
@@ -10,11 +10,11 @@ def fvc_is_ready(camera='SCI'):
     kpffvc = ktl.cache('kpffvc')
     kpfpower = ktl.cache('kpfpower')
     tests = [kpfpower[f'OUTLET_{outlet}'].read() == 'On',
-             kpffvc[f'{camera}AUTOEXP'].read() > 'Off',
-             kpffvc[f'{camera}AUTOGAIN'].read() > 'Off',
-             kpffvc[f'{camera}BLCLAMP'].read() > 'Disabled',
+             kpffvc[f'{camera}AUTOEXP'].read() == 'Off',
+             kpffvc[f'{camera}AUTOGAIN'].read() == 'Off',
+             kpffvc[f'{camera}BLCLAMP'].read() == 'Disabled',
              abs(1.0-kpffvc[f'{camera}BLACKLEVEL'].read(binary=True)) < 0.001,
-             kpffvc[f'{camera}GENABLE'].read() > 'Disabled',
+             kpffvc[f'{camera}GENABLE'].read() == 'Disabled',
              abs(1.0-kpffvc[f'{camera}GAMMA'].read(binary=True)) < 0.001,
              ]
     return np.all(np.array(tests))
