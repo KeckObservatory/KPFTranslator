c46cba0e9cb64aab9d7bc9f961cd8407e2cbf041
Josh Walawender <jmwalawender@gmail.com>
2022-10-12 11:22:14 -1000

    Add stubs for the DDOI script elements

---
+++ kpf/ddoi_script_functions/waitfor_configure_science.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+
+
+class WaitForConfigureScience(KPFTranslatorFunction):
+    '''
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        raise NotImplementedError()
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def add_cmdline_args(cls, parser, cfg=None):
+        """
+        The arguments to add to the command line interface.
+        """
+        args_to_add = OrderedDict()
+#         args_to_add['x'] = {'type': float,
+#                             'help': ''}
+        parser = cls._add_args(parser, args_to_add, print_only=False)
+        return super().add_cmdline_args(parser, cfg)
