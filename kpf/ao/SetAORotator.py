dfa6350974d27faedd7f0d45c61454f982c66e92
Josh Walawender <jmwalawender@gmail.com>
2022-10-16 12:04:41 -1000

    add cmdline args

--- kpf/ao/SetAORotator.py
+++ kpf/ao/SetAORotator.py
@@ -20,3 +20,12 @@ class SetAORotator(KPFTranslatorFunction):
     def post_condition(cls, args, logger, cfg):
         ao = ktl.cache('ao')
         return ktl.waitfor('($ao.OBRTSTST == INPOS)', timeout=180)
+
+    @classmethod
+    def add_cmdline_args(cls, parser, cfg=None):
+        args_to_add = OrderedDict()
+        args_to_add['dest'] = {'type': float,
+                               'help': 'Desired rotator position'}
+
+        parser = cls._add_args(parser, args_to_add, print_only=False)
+        return super().add_cmdline_args(parser, cfg)
