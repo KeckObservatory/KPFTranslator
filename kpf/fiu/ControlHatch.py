9ca6e44a7dbbf3568c2d18b3e739732a8fcc7c0c
Josh Walawender <jmwalawender@gmail.com>
2022-10-28 08:48:55 -1000

    cleanup

--- kpf/fiu/ControlHatch.py
+++ kpf/fiu/ControlHatch.py
@@ -30,13 +30,9 @@ class ControlHatch(KPFTranslatorFunction):
 
     @classmethod
     def add_cmdline_args(cls, parser, cfg=None):
-        """
-        The arguments to add to the command line interface.
-        """
         args_to_add = OrderedDict()
         args_to_add['destination'] = {'type': str,
                                 'help': 'Desired hatch position: "open" or "closed"'}
 
         parser = cls._add_args(parser, args_to_add, print_only=False)
         return super().add_cmdline_args(parser, cfg)
-
