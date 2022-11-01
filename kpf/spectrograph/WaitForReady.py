fc75008504fcbb3d3afa80d69f0bb24365098537
Josh Walawender <jmwalawender@gmail.com>
2022-11-01 09:07:16 -1000

    Remove checking of last file

--- kpf/spectrograph/WaitForReady.py
+++ kpf/spectrograph/WaitForReady.py
@@ -48,15 +48,15 @@ class WaitForReady(KPFTranslatorFunction):
         wait_logic += '($kpfexpose.EXPOSE == 0)'
         log.debug(f"  Waiting ({wait_time:.0f}s max) for detectors to be ready")
         success = ktl.waitFor(wait_logic, timeout=wait_time)
-        if success is True:
-            if 'Green' in detector_list:
-                lastfile = ktl.cache('kpfgreen', 'FITSFILE').read()
-                if Path(lastfile).exists():
-                    log.debug(f"  Found Green FITSFILE: {lastfile}")
-            if 'Red' in detector_list:
-                lastfile = ktl.cache('kpfred', 'FITSFILE').read()
-                if Path(lastfile).exists():
-                    log.debug(f"  Found Red FITSFILE:   {lastfile}")
+#         if success is True:
+#             if 'Green' in detector_list:
+#                 lastfile = ktl.cache('kpfgreen', 'FITSFILE').read()
+#                 if Path(lastfile).exists():
+#                     log.debug(f"  Found Green FITSFILE: {lastfile}")
+#             if 'Red' in detector_list:
+#                 lastfile = ktl.cache('kpfred', 'FITSFILE').read()
+#                 if Path(lastfile).exists():
+#                     log.debug(f"  Found Red FITSFILE:   {lastfile}")
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
