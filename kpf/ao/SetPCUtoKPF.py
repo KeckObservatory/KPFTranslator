45b9f090f10498434aeccf584862d57ceef3d170
Josh Walawender <joshwalawender@me.com>
2022-11-09 14:05:18 -1000

    Use safe PCU stage logic

--- kpf/ao/SetPCUtoKPF.py
+++ kpf/ao/SetPCUtoKPF.py
@@ -1,7 +1,8 @@
 import ktl
 
 from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
-from .. import log
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
 
 
 class SetPCUtoKPF(KPFTranslatorFunction):
@@ -17,10 +18,19 @@ class SetPCUtoKPF(KPFTranslatorFunction):
         log.debug(f"Setting PCU to KPF Position")
         log.debug(f'  Move PCU in LZ direction')
         ao['PCSFLZ'].write('0')
+        success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+        if success is False:
+            raise FailedToReachDestination(ao['PCSFLZ'].read(), 0)
         log.debug(f'  Move PCU in X direction')
         ao['PCSFX'].write('80')
+        success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+        if success is False:
+            raise FailedToReachDestination(ao['PCSFX'].read(), 80)
         log.debug(f'  Move PCU in Y direction')
         ao['PCSFY'].write('45')
+        success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+        if success is False:
+            raise FailedToReachDestination(ao['PCSFY'].read(), 45)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
