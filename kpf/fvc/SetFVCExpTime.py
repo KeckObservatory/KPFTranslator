0823f57cba5cefce55b3f6716b80d2f6080649fb
Josh Walawender <joshwalawender@me.com>
2022-11-10 10:08:09 -1000

    use better input checking and raise exceptions

--- kpf/fvc/SetFVCExpTime.py
+++ kpf/fvc/SetFVCExpTime.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import ktl
 
 from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
-
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
 from . import fvc_is_ready
 
 
@@ -12,27 +13,34 @@ class SetFVCExpTime(KPFTranslatorFunction):
     '''
     @classmethod
     def pre_condition(cls, args, logger, cfg):
-        camera = args.get('camera', 'SCI')
-        return fvc_is_ready(camera=camera)
+        check_input(args, 'camera', allowed_values=['SCI', 'CAHK', 'CAL', 'EXT'])
+        camera = args.get('camera')
+        if fvc_is_ready(camera=camera) is not True:
+            raise FailedPreCondition(f"Camera {camera} is not ready")
+        check_input(args, 'exptime', value_min=0.001, value_max=60)
+        return True
 
     @classmethod
     def perform(cls, args, logger, cfg):
-        camera = args.get('camera', 'SCI')
+        camera = args.get('camera')
         kpffvc = ktl.cache('kpffvc')
-        exptime = args.get('exptime', None)
-        if exptime is not None:
-            kpffvc[f'{camera}EXPTIME'].write(exptime)
+        exptime = args.get('exptime')
+        log.debug(f"Setting {camera} FVC exposure time to {exptime:.3f} s")
+        kpffvc[f'{camera}EXPTIME'].write(exptime)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
-        camera = args.get('camera', 'SCI')
-        exptime = args.get('exptime', None)
-        if exptime is not None:
-            timeout = cfg.get('times', 'fvc_command_timeout', fallback=5)
-            tol = cfg.get('tolerances', 'guider_exptime_tolerance', fallback=0.01)
-            expr = (f'($kpffvc.{camera}EXPTIME > {exptime}-{tol}) '\
-                    f'and ($kpffvc.{camera}EXPTIME < {exptime}+{tol})')
-            return ktl.waitfor(expr, timeout=timeout)
+        camera = args.get('camera')
+        exptime = args.get('exptime')
+        timeout = cfg.get('times', 'fvc_command_timeout', fallback=5)
+        tol = cfg.get('tolerances', 'guider_exptime_tolerance', fallback=0.01)
+        expr = (f'($kpffvc.{camera}EXPTIME > {exptime}-{tol}) '\
+                f'and ($kpffvc.{camera}EXPTIME < {exptime}+{tol})')
+        success = ktl.waitfor(expr, timeout=timeout)
+        if success is not True:
+            exptimekw = ktl.cache('kpffvc', f"{camera}EXPTIME")
+            raise FailedToReachDestination(exptimekw.read(), exptime)
+        return True
 
     @classmethod
     def add_cmdline_args(cls, parser, cfg=None):
