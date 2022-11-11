959a39364107de79bc3bfd49ce1c4d1ade8fd1bd
Josh Walawender <joshwalawender@me.com>
2022-11-10 14:23:53 -1000

    Use named position for PCU stage

--- kpf/ao/SetPCUtoKPF.py
+++ kpf/ao/SetPCUtoKPF.py
@@ -15,22 +15,35 @@ class SetPCUtoKPF(KPFTranslatorFunction):
     @classmethod
     def perform(cls, args, logger, cfg):
         ao = ktl.cache('ao')
-        log.debug(f"Setting PCU to KPF Position")
-        log.debug(f'  Move PCU in LZ direction')
-        ao['PCSFLZ'].write('0')
+        # First send it home
+        log.info(f"Sending PCU to Home")
+        ao['PCSFNAME'].write('Home')
         success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
         if success is False:
-            raise FailedToReachDestination(ao['PCSFLZ'].read(), 0)
-        log.debug(f'  Move PCU in X direction')
-        ao['PCSFX'].write('80')
+            raise FailedToReachDestination(ao['PCSFNAME'].read(), 'Home')
+        # Second send it to KPF
+        log.info(f"Sending PCU to KPF")
+        ao['PCSFNAME'].write('KPF')
         success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
         if success is False:
-            raise FailedToReachDestination(ao['PCSFX'].read(), 80)
-        log.debug(f'  Move PCU in Y direction')
-        ao['PCSFY'].write('45')
-        success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
-        if success is False:
-            raise FailedToReachDestination(ao['PCSFY'].read(), 45)
+            raise FailedToReachDestination(ao['PCSFNAME'].read(), 'KPF')
+
+#         log.debug(f"Setting PCU to KPF Position")
+#         log.debug(f'  Move PCU in LZ direction')
+#         ao['PCSFLZ'].write('0')
+#         success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+#         if success is False:
+#             raise FailedToReachDestination(ao['PCSFLZ'].read(), 0)
+#         log.debug(f'  Move PCU in X direction')
+#         ao['PCSFX'].write('80')
+#         success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+#         if success is False:
+#             raise FailedToReachDestination(ao['PCSFX'].read(), 80)
+#         log.debug(f'  Move PCU in Y direction')
+#         ao['PCSFY'].write('45')
+#         success = ktl.waitfor("($ao.PCSFSTST == INPOS)", timeout=120)
+#         if success is False:
+#             raise FailedToReachDestination(ao['PCSFY'].read(), 45)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
