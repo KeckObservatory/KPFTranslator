cd8eb43f222b616f2ee321cef70416e67b5a715d
Josh Walawender <jmwalawender@gmail.com>
2022-11-20 19:27:20 -1000

    Use OB instead of args

--- kpf/scripts/TestScienceADC.py
+++ kpf/scripts/TestScienceADC.py
@@ -10,7 +10,8 @@ import ktl
 import keygrabber
 
 from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
-
+from .. import (KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
 from ..fvc.TakeFVCExposure import TakeFVCExposure
 from ..guider.TakeGuiderExposure import TakeGuiderExposure
 
@@ -47,6 +48,23 @@ class TestScienceADC(KPFTranslatorFunction):
     '''
     @classmethod
     def pre_condition(cls, args, logger, cfg):
+        # Use file input for OB instead of args (temporary)
+        check_input(args, 'OBfile')
+        OBfile = Path(args.get('OBfile')).expanduser()
+        if OBfile.exists() is True:
+            OB = yaml.safe_load(open(OBfile, 'r'))
+            log.warning(f"Using OB information from file {OBfile}")
+        check_input(OB, 'Template_Name', allowed_values=['kpf_eng_testsciadc'])
+        check_input(OB, 'Template_Version', allowed_values=['0.3'])
+        check_input(OB, 'nx')
+        check_input(OB, 'ny')
+        check_input(OB, 'dx')
+        check_input(OB, 'dy')
+        check_input(OB, 'cameras')
+        cameras = OB.get('cameras', '').split(',')
+        for camera in cameras:
+            if camera not in ['CRED2', 'SCI', 'CAHK', 'EXT', 'ExpMeter']:
+                raise FailedPreCondition(f"Camera {camera} not supported")
         return True
 
     @classmethod
@@ -56,13 +74,21 @@ class TestScienceADC(KPFTranslatorFunction):
         log.info(f"args = {args}")
         log.info("###########")
 
-        images_file = Path(f'~/logs/{this_file_name}_images_{now_str}.txt').expanduser()
-        fluxes_file = Path(f'~/logs/{this_file_name}_fluxes_{now_str}.txt').expanduser()
+        images_file = log_dir / Path(f'{this_file_name}_images_{now_str}.txt')
+        fluxes_file = log_dir / Path(f'{this_file_name}_fluxes_{now_str}.txt')
 
         images = Table(names=('file', 'camera', 'dx', 'dy'),
                        dtype=('a90',  'a10',    'f4', 'f4'))
-        expmeter_flux = Table(names=('i', 'j', 'dx', 'dy', 'f1', 'f2', 'f3', 'f4', 'nimages'),
-                              dtype=('i4', 'i4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'i4'))
+        expmeter_flux = Table(names=('i', 'j', 'dx', 'dy',
+                                     'bck1', 'bck2', 'bck3', 'bck4',
+                                     'cur1', 'cur2', 'cur3', 'cur4',
+                                     'raw1', 'raw2', 'raw3', 'raw4',
+                                     'nimages'),
+                              dtype=('i4', 'i4', 'f4', 'f4',
+                                     'f4', 'f4', 'f4', 'f4',
+                                     'f4', 'f4', 'f4', 'f4',
+                                     'f4', 'f4', 'f4', 'f4',
+                                     'i4'))
 
         nx = args.get('nx', 3)
         ny = args.get('ny', 3)
@@ -100,14 +126,13 @@ class TestScienceADC(KPFTranslatorFunction):
                 log.info(f"Offsetting to position ({xs[i]}, {ys[j]})")
                 kpffiu['ADC1VAL'].write(nominalx + xs[i])
                 kpffiu['ADC2VAL'].write(nominaly + ys[j])
-                cond = '($kpffiu.ADC1STA == Ready) and ($kpffiu.ADC2STA == Ready)'
-                success = ktl.waitFor(cond, timeout=2*max([dx*nx, dy*ny])/5)
+                expr = '($kpffiu.ADC1STA == Ready) and ($kpffiu.ADC2STA == Ready)'
+                success = ktl.waitFor(expr, timeout=2*max([dx*nx, dy*ny])/5)
                 if success is not True:
                     ADC1STA = kpffiu['ADC1STA'].read()
                     ADC2STA = kpffiu['ADC2STA'].read()
                     msg = f'Timed out waiting for ADCs: ADC1STA={ADC1STA} ADC2STA={ADC2STA}'
-                    log.error(msg)
-                    raise Exception(msg)
+                    raise KPFException(msg)
 
                 # Start Exposure Meter
                 if 'ExpMeter' in args.get('cameras', ''):
@@ -152,19 +177,27 @@ class TestScienceADC(KPFTranslatorFunction):
                         images.add_row(row)
 
                 # Stop Exposure Meter
-                if 'ExpMeter' in args.get('cameras', '').split(','):
-                    log.info(f"  Stopping exposure meter")
-                    kpf_expmeter['EXPOSE'].write('end')
-                    ktl.waitFor('$kpf_expmeter.EXPSTATE == Ready')
-                    kpf_expmeter['CUR_COUNTS'].wait()
+                log.info(f"  Waiting for kpfexpose readout to start")
+                WaitForReadout.execute({})
+                log.info(f"  Waiting for ExpMeter DRP")
+                kpf_expmeter['CUR_COUNTS'].wait()
+                log.info(f"  Waiting for ExpMeter to be Ready")
+                EMsuccess = ktl.waitFor('$kpf_expmeter.EXPSTATE == Ready')
+                if EMsuccess is True:
                     lastfile = kpf_expmeter['FITSFILE'].read()
+                else:
+                    lastfile = 'failed'
                     row = {'file': lastfile, 'camera': 'ExpMeter',
                            'dx': xs[i], 'dy': ys[j]}
-                    images.add_row(row)
-                    # Retrieve keyword history
-                    end = time.time()
-                    log.info(f"  Retrieving keyword history")
-                    kws = {'kpf_expmeter': ['CUR_COUNTS']}
+                images.add_row(row)
+                # Retrieve keyword history
+                end = time.time()
+                expmeter_data = {'dx': xs[i], 'dy': ys[j],
+                                 'i': i, 'j': j,
+                                 }
+                for counts_kw in ['CUR_COUNTS', 'RAW_COUNTS', 'BCK_COUNTS']:
+                    log.info(f"  Retrieving keyword history for {counts_kw}")
+                    kws = {'kpf_expmeter': [counts_kw]}
                     counts_history = keygrabber.retrieve(kws, begin=begin, end=end)
                     # Extract counts and save to table
                     fluxes = np.zeros((len(counts_history), 4))
@@ -174,14 +207,12 @@ class TestScienceADC(KPFTranslatorFunction):
                         log.debug(f"  {ts}: {value_floats}")
                         fluxes[k] = value_floats
                     avg_fluxes = np.mean(fluxes, axis=0)
-                    expmeter_flux.add_row({'dx': xs[i], 'dy': ys[j],
-                                           'i': i, 'j': j,
-                                           'f1': avg_fluxes[0],
-                                           'f2': avg_fluxes[1],
-                                           'f3': avg_fluxes[2],
-                                           'f4': avg_fluxes[3],
-                                           'nimages': len(counts_history),
-                                           })
+                    expmeter_data[f"{counts_kw[:3].lower()}1"] = avg_fluxes[0]
+                    expmeter_data[f"{counts_kw[:3].lower()}2"] = avg_fluxes[1]
+                    expmeter_data[f"{counts_kw[:3].lower()}3"] = avg_fluxes[2]
+                    expmeter_data[f"{counts_kw[:3].lower()}4"] = avg_fluxes[3]
+                expmeter_data['nimages'] = len(counts_history)
+                expmeter_flux.add_row(expmeter_data)
 
             if images_file.exists():
                 images_file.unlink()
@@ -199,7 +230,7 @@ class TestScienceADC(KPFTranslatorFunction):
                 fluxes_file.unlink()
             expmeter_flux.write(fluxes_file, format='ascii.csv')
 
-        # Send tip tilt back to 0,0
+        # Send ADC back to nomimal
         kpffiu['ADCTRACK'].write('On')
 
 
@@ -213,18 +244,9 @@ class TestScienceADC(KPFTranslatorFunction):
         '''
         from collections import OrderedDict
         args_to_add = OrderedDict()
-        args_to_add['nx'] = {'type': int,
-                    'help': 'Number of grid points for ADC prism 1'}
-        args_to_add['ny'] = {'type': int,
-                    'help': 'Number of grid points for ADC prism 2'}
-        args_to_add['dx'] = {'type': float,
-                    'help': 'Distance (arcsec) between grid points for ADC prism 1'}
-        args_to_add['dy'] = {'type': float,
-                    'help': 'Distance (arcsec) between grid points for ADC prism 2'}
-        args_to_add['cameras'] = {'type': str,
-                    'help': 'List of cameras'}
-        args_to_add['comment'] = {'type': str,
-                    'help': 'Comment for log'}
-
+        args_to_add['OBfile'] = {'type': str,
+                                 'help': ('A YAML fortmatted file with the OB '
+                                          'to be executed. Will override OB '
+                                          'data delivered as args.')}
         parser = cls._add_args(parser, args_to_add, print_only=False)
         return super().add_cmdline_args(parser, cfg)
