d4f0e2aa90e0b9f7296922fa7b095a343f75bc03
Josh Walawender <joshwalawender@me.com>
2022-12-07 19:33:23 -1000

    Be able to specify which DISPLAY

--- kpf/scripts/StartUp.py
+++ kpf/scripts/StartUp.py
@@ -1,3 +1,4 @@
+import os
 import time
 import subprocess
 
@@ -15,10 +16,13 @@ from ..spectrograph.WaitForReady import WaitForReady
 from .SetObserverFromSchedule import SetObserverFromSchedule
 
 
-def get_window_list():
-    wmctrl_list_cmd = ['wmctrl', '-l']
-    wmctrl_list_proc = subprocess.run(wmctrl_list_cmd, stdout=subprocess.PIPE)
-    wmctrl_list = wmctrl_list_proc.stdout.decode().split('\n')
+def get_window_list(env=None):
+    wmctrl_cmd = ['wmctrl', '-l']
+    if env is not None:
+        wmctrl_proc = subprocess.run(wmctrl_cmd, stdout=subprocess.PIPE, env=env)
+    else:
+        wmctrl_proc = subprocess.run(wmctrl_cmd, stdout=subprocess.PIPE)
+    wmctrl_list = wmctrl_proc.stdout.decode().split('\n')
     window_names = [w[18:].strip() for w in wmctrl_list]
     return window_names
 
@@ -51,14 +55,24 @@ class StartUp(KPFTranslatorFunction):
         SetProgram.execute(args)
         SetObserverFromSchedule.execute(args)
 
+        # Get DISPLAY varibales
+        env = os.environ
+        uidisp = {}
+        for dispno in [0, 1, 2, 3]:
+            uidisp_proc = subprocess.run(['uidisp', f'{dispno}'], env=env,
+                                         stdout=subprocess.PIPE)
+            uidisp[dispno] = uidisp_proc.stdout.decode().strip('\n')
+
         # Find out of GUIs are Running
         window_names = get_window_list()
 
         GUIs = [{'name': 'KPF Fiber Injection Unit (FIU)',
                  'cmd': '/kroot/rel/default/bin/fiu_gui',
-                 'position': '0,5,170,-1,-1'},
+                 'dispno': 0,
+                 'position': '0,5,165,-1,-1'},
                  {'name': 'KPF Exposure Meter',
                  'cmd': '/kroot/rel/default/bin/expmeter_gui',
+                 'dispno': 0,
                  'position': '0,5,665,-1,-1'},
                  ]
 
@@ -67,15 +81,16 @@ class StartUp(KPFTranslatorFunction):
             GUIname = GUI['name']
             if GUIname not in window_names:
                 log.info(f"Starting {GUIname} GUI")
-                gui_proc = subprocess.Popen([GUI['cmd']])
-                window_names = get_window_list()
+                env['DISPLAY'] = uidisp[GUI['dispno']]
+                gui_proc = subprocess.Popen([GUI['cmd']], env=env)
+                window_names = get_window_list(env=env)
                 while GUI['name'] not in window_names:
                     log.info(f"Waiting for {GUIname} to appear")
                     time.sleep(2)
-                    window_names = get_window_list()
+                    window_names = get_window_list(env=env)
                 log.info(f"Positioning {GUIname} GUI")
                 wmctrl_cmd = ['wmctrl', '-r', f'"{GUIname}"', '-e', GUI['position']]
-                wmctrl_proc = subprocess.run(' '.join(wmctrl_cmd), shell=True)
+                wmctrl_proc = subprocess.run(' '.join(wmctrl_cmd), env=env, shell=True)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
