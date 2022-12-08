edb247cea8bb4c579f3495bd23b736e9bfa5bb3b
Josh Walawender <joshwalawender@me.com>
2022-12-07 18:39:10 -1000

    Position GUIs

--- kpf/scripts/StartUp.py
+++ kpf/scripts/StartUp.py
@@ -1,3 +1,4 @@
+import time
 import subprocess
 
 import ktl
@@ -12,6 +13,14 @@ from .SetOutdirs import SetOutdirs
 from ..spectrograph.SetProgram import SetProgram
 from ..spectrograph.WaitForReady import WaitForReady
 from .SetObserverFromSchedule import SetObserverFromSchedule
+
+
+def get_window_list():
+    wmctrl_list_cmd = ['wmctrl', '-l']
+    wmctrl_list_proc = subprocess.run(wmctrl_list_cmd, stdout=subprocess.PIPE)
+    wmctrl_list = wmctrl_list_proc.stdout.decode().split('\n')
+    window_names = [w[18:].strip() for w in wmctrl_list]
+    return window_names
 
 
 class StartUp(KPFTranslatorFunction):
@@ -33,16 +42,40 @@ class StartUp(KPFTranslatorFunction):
     @classmethod
     def perform(cls, args, logger, cfg):
         # Set Outdirs
-        WaitForReady.execute({})
+        expose = ktl.cache('kpfexpose', 'EXPOSE')
+        if expose.read() != 'Ready':
+            log.info('Waiting for kpfexpose to be Ready')
+            WaitForReady.execute({})
         SetOutdirs.execute({})
         # Set progname and observer
         SetProgram.execute(args)
         SetObserverFromSchedule.execute(args)
-        # Start GUIs
-        log.info("Starting FIU Status GUI")
-        fiugui_proc = subprocess.Popen(['/kroot/rel/default/bin/fiu_gui'])
-        log.info("Starting Exposure Meter GUI")
-        emgui_proc = subprocess.Popen(['/kroot/rel/default/bin/expmeter_gui'])
+
+        # Find out of GUIs are Running
+        window_names = get_window_list()
+
+        GUIs = [{'name': 'KPF Fiber Injection Unit (FIU)',
+                 'cmd': '/kroot/rel/default/bin/fiu_gui',
+                 'position': '0,5,170,-1,-1'},
+                 {'name': 'KPF Exposure Meter',
+                 'cmd': '/kroot/rel/default/bin/expmeter_gui',
+                 'position': '0,5,665,-1,-1'},
+                 ]
+
+        # Start GUIs if needed
+        for GUI in GUIs:
+            GUIname = GUI['name']
+            if GUIname not in window_names:
+                log.info(f"Starting {GUIname} GUI")
+                gui_proc = subprocess.Popen([GUI['cmd']])
+                window_names = get_window_list()
+                while GUI['name'] not in window_names:
+                    log.info(f"Waiting for {GUIname} to appear")
+                    time.sleep(2)
+                    window_names = get_window_list()
+                log.info(f"Positioning {GUIname} GUI")
+                wmctrl_cmd = ['wmctrl', '-r', f'"{GUIname}"', '-e', GUI['position']]
+                wmctrl_proc = subprocess.run(' '.join(wmctrl_cmd), shell=True)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
