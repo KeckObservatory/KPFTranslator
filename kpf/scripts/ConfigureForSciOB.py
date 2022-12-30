a2ff5bc1c959002b4f6243d66c424e43711a5b27
Josh Walawender <joshwalawender@me.com>
2022-12-29 19:35:48 -1000

    fix

--- kpf/scripts/ConfigureForSciOB.py
+++ kpf/scripts/ConfigureForSciOB.py
@@ -32,7 +32,7 @@ class ConfigureForSciOB(KPFTranslatorFunction):
     @classmethod
     @obey_scriptrun
     def pre_condition(cls, OB, logger, cfg):
-        check_input(OB, 'Template_Name', allowed_values=['kpf_cal'])
+        check_input(OB, 'Template_Name', allowed_values=['kpf_sci'])
         check_input(OB, 'Template_Version', version_check=True, value_min='0.3')
         return True
 
