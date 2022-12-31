9f84cb0ac7280cc9fb07cc26ffc5f65223d3dd5f
Josh Walawender <joshwalawender@me.com>
2022-12-30 21:35:55 -1000

    Rebuild from FiberGridSearch

--- kpf/scripts/TestScienceADC.py
+++ kpf/scripts/TestScienceADC.py
@@ -1,7 +1,9 @@
+import os
 from pathlib import Path
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
+import yaml
 
 import numpy as np
 from astropy.table import Table, Row
@@ -13,8 +15,17 @@ from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
 from .. import (KPFException, FailedPreCondition, FailedPostCondition,
                 FailedToReachDestination, check_input)
 from . import register_script, obey_scriptrun, check_scriptstop
+from ..fiu.SetTipTiltTargetPixel import SetTipTiltTargetPixel
 from ..fvc.TakeFVCExposure import TakeFVCExposure
+from ..fvc.SetFVCExpTime import SetFVCExpTime
 from ..guider.TakeGuiderExposure import TakeGuiderExposure
+from ..spectrograph.SetExptime import SetExptime
+from ..spectrograph.StartExposure import StartExposure
+from ..spectrograph.WaitForReady import WaitForReady
+from ..spectrograph.WaitForReadout import WaitForReadout
+from ..spectrograph.SetSourceSelectShutters import SetSourceSelectShutters
+from ..spectrograph.SetTimedShutters import SetTimedShutters
+from ..spectrograph.SetTriggeredDetectors import SetTriggeredDetectors
 
 
 ##-------------------------------------------------------------------------
@@ -32,9 +43,14 @@ LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s',
 LogConsoleHandler.setFormatter(LogFormat)
 log.addHandler(LogConsoleHandler)
 ## Set up file output
-now = datetime.utcnow()
-now_str = now.strftime('%Y%m%dat%H%M%S')
-LogFileName = Path(f'~/logs/{this_file_name}_{now_str}.log').expanduser()
+utnow = datetime.utcnow()
+now_str = utnow.strftime('%Y%m%dat%H%M%S')
+date = utnow-timedelta(days=1)
+date_str = date.strftime('%Y%b%d').lower()
+log_dir = Path(f"/s/sdata1701/{os.getlogin()}/{date_str}/script_logs/")
+if log_dir.exists() is False:
+    log_dir.mkdir(parents=True)
+LogFileName = log_dir / f"{this_file_name}_{now_str}.log"
 LogFileHandler = logging.FileHandler(LogFileName)
 LogFileHandler.setLevel(logging.DEBUG)
 LogFileHandler.setFormatter(LogFormat)
@@ -50,17 +66,18 @@ class TestScienceADC(KPFTranslatorFunction):
     @classmethod
     @obey_scriptrun
     def pre_condition(cls, OB, logger, cfg):
-        check_input(OB, 'Template_Name', allowed_values=['kpf_eng_testsciadc'])
-        check_input(OB, 'Template_Version', version_check=True, value_min='0.3')
+        check_input(OB, 'Template_Name', allowed_values=['kpf_eng_fgs'])
+        check_input(OB, 'Template_Version', version_check=True, value_min='0.4')
         check_input(OB, 'nx')
         check_input(OB, 'ny')
         check_input(OB, 'dx')
         check_input(OB, 'dy')
-        check_input(OB, 'cameras')
-        cameras = OB.get('cameras', '').split(',')
-        for camera in cameras:
-            if camera not in ['CRED2', 'SCI', 'CAHK', 'EXT', 'ExpMeter']:
-                raise FailedPreCondition(f"Camera {camera} not supported")
+        check_input(OB, 'FVCs')
+        check_input(OB, 'ExpMeter_exptime')
+        FVCs = OB.get('FVCs', '').split(',')
+        for FVC in FVCs:
+            if FVC not in ['SCI', 'CAHK', 'EXT']:
+                raise FailedPreCondition(f"FVC {FVC} not supported")
         return True
 
     @classmethod
@@ -88,10 +105,11 @@ class TestScienceADC(KPFTranslatorFunction):
                                      'f4', 'f4', 'f4', 'f4',
                                      'i4'))
 
-        nx = args.get('nx')
-        ny = args.get('ny')
-        dx = args.get('dx')
-        dy = args.get('dy')
+        FVCs = OB.get('FVCs', '').split(',')
+        nx = OB.get('nx')
+        ny = OB.get('ny')
+        dx = OB.get('dx')
+        dy = OB.get('dy')
         xis = [xi for xi in range(int(-nx/2),int((nx+1)/2),1)]
         yis = [yi for yi in range(int(-ny/2),int((ny+1)/2),1)]
         xs = [xi*dx for xi in xis]
@@ -101,26 +119,36 @@ class TestScienceADC(KPFTranslatorFunction):
         kpffiu = ktl.cache('kpffiu')
         kpffiu['ADCTRACK'].write('Off')
 
-        # Set up Exposure Meter
-        if 'ExpMeter' in args.get('cameras', ''):
-            # Set up kpfexpose
-            kpfexpose = ktl.cache('kpfexpose')
-            SetSourceSelectShutters.execute({'SSS_Science': True, 'SSS_Sky': True})
-            SetTimedShutters.execute({'TimedShutter_Scrambler': True})
-            SetTriggeredDetectors.execute({'TriggerExpMeter': True})
+        # Set up guider (assume parameters set during acquisition of star)
+        kpfguide = ktl.cache('kpfguide')
+
+        # Set up kpfexpose
+        kpfexpose = ktl.cache('kpfexpose')
+        SetSourceSelectShutters.execute({'SSS_Science': True, 'SSS_Sky': True})
+        SetTimedShutters.execute({'TimedShutter_Scrambler': True})
+        SetTriggeredDetectors.execute({'TriggerExpMeter': True})
+        total_exptime = ExpMeter_exptime = OB.get('TimeOnPosition')
+        SetExptime.execute({'Exptime': total_exptime})
+
+        # Configure Exposure Meter
+        kpf_expmeter = ktl.cache('kpf_expmeter')
+        ExpMeter_exptime = OB.get('ExpMeter_exptime')
+        log.info(f"Setting kpf_expmeter.EXPOSURE = {ExpMeter_exptime:.2f} s")
+        kpf_expmeter['EXPOSURE'].write(ExpMeter_exptime)
 
         # Set up FVCs
         kpffvc = ktl.cache('kpffvc')
-
-        # Set up guider
-        if 'CRED2' in args.get('cameras', ''):
-            kpfguide = ktl.cache('kpfguide')
+        for FVC in ['SCI', 'CAHK', 'EXT']:
+            if FVC in FVCs and OB.get(f'{FVC}FVC_exptime', None) != None:
+                exptime = OB.get(f'{FVC}FVC_exptime')
+                log.info(f"Setting {FVC} FVC Exptime = {exptime:.2f} s")
+                SetFVCExpTime.execute({'camera': FVC, 'exptime': exptime})
 
         for i,xi in enumerate(xis):
             for j,yi in enumerate(yis):
                 # Offset to position
                 nominalx, nominaly = kpffiu['ADCPRISMS'].read(binary=True)
-                log.info(f"Offsetting to position ({xs[i]}, {ys[j]})")
+                log.info(f"Offsetting to ({xs[i]:.2f}, {ys[j]:.2f}) ({xis[i]}, {yis[j]})")
                 kpffiu['ADC1VAL'].write(nominalx + xs[i])
                 kpffiu['ADC2VAL'].write(nominaly + ys[j])
                 expr = '($kpffiu.ADC1STA == Ready) and ($kpffiu.ADC2STA == Ready)'
@@ -131,61 +159,76 @@ class TestScienceADC(KPFTranslatorFunction):
                     msg = f'Timed out waiting for ADCs: ADC1STA={ADC1STA} ADC2STA={ADC2STA}'
                     raise KPFException(msg)
 
-                # Start Exposure Meter
-                if 'ExpMeter' in args.get('cameras', ''):
-                    log.info(f"  Starting exposure meter")
-                    kpf_expmeter['OBJECT'].write(f'Grid search {xs[i]}, {ys[j]} arcsec')
-                    exptime = kpf_expmeter['EXPOSURE'].read(binary=True)
-                    ktl.waitFor('($kpf_expmeter.EXPOSE == Ready)', timeout=exptime+2)
-                    kpf_expmeter['EXPOSE'].write('Start')
-                    # Begin timestamp for history retrieval
-                    begin = time.time()
+                # Take Exposure to make sure we wait at least one cycle
+                log.info(f"Taking extra guider exposure to wait one cycle")
+                TakeGuiderExposure.execute({}) # Blocks until done
+
+                sleep_time = 5
+                log.info(f"Sleeping {sleep_time} s to allow tip tilt loop to settle")
+                time.sleep(sleep_time)
+
+                # Start Exposure Meter and Science Cameras
+                WaitForReady.execute({})
+                kpfexpose['OBJECT'].write(f'Grid search {xs[i]}, {ys[j]} arcsec')
+                log.info(f"Starting kpfexpose cameras")
+                StartExposure.execute({})
+                # Begin timestamp for history retrieval
+                begin = time.time()
 
                 # Start FVC Exposures
                 initial_lastfile = {}
-                for camera in ['SCI', 'CAHK', 'CAL', 'EXT']:
-                    if camera in args.get('cameras', '').split(','):
-                        initial_lastfile[camera] = kpffvc[f"{camera}LASTFILE"].read()
-                        log.debug(f"  Initial lastfile for {camera} = {initial_lastfile[camera]}")
-                        log.info(f"  Starting {camera} FVC exposure")
-                        TakeFVCExposure.execute({'camera': camera, 'wait': False})
+                for FVC in ['SCI', 'CAHK', 'EXT']:
+                    if FVC in FVCs:
+                        initial_lastfile[FVC] = kpffvc[f"{FVC}LASTFILE"].read()
+                        log.debug(f"  Initial lastfile for {FVC} = {initial_lastfile[FVC]}")
+                        log.info(f"  Starting {FVC} FVC exposure")
+                        TakeFVCExposure.execute({'camera': FVC, 'wait': False})
 
                 # Expose using CRED2
-                if 'CRED2' in args.get('cameras', '').split(','):
-                    log.info(f"  Taking CRED2 exposure")
-                    TakeGuiderExposure.execute({})
-                    lastfile = kpfguide[f"LASTFILE"].read()
-                    row = {'file': lastfile, 'camera': 'CRED2',
-                           'dx': xs[i], 'dy': ys[j]}
-                    images.add_row(row)
+                log.info(f"  Taking CRED2 exposure")
+                TakeGuiderExposure.execute({}) # Blocks until done
+                lastfile = kpfguide[f"LASTFILE"].read()
+                row = {'file': lastfile, 'camera': 'CRED2',
+                       'dx': xs[i], 'dy': ys[j]}
+                images.add_row(row)
+
+                # Here's where we wait for the duration of the FVC exposures
 
                 # Collect files for FVC exposures
-                for camera in ['SCI', 'CAHK', 'CAL', 'EXT']:
-                    if camera in args.get('cameras', '').split(','):
-                        log.info(f"  Looking for output file for {camera}")
-                        expr = f'($kpffvc.{camera}LASTFILE != "{initial_lastfile[camera]}")'
+                for FVC in ['SCI', 'CAHK', 'EXT']:
+                    if FVC in FVCs:
+                        log.info(f"  Looking for output file for {FVC}")
+                        expr = f'($kpffvc.{FVC}LASTFILE != "{initial_lastfile[FVC]}")'
                         log.debug(f"  Waiting for: {expr}")
-                        ktl.waitFor(expr, timeout=20)
-                        lastfile = kpffvc[f'{camera}LASTFILE'].read()
-                        log.debug(f"Found {lastfile}")
-                        row = {'file': lastfile, 'camera': camera,
-                               'dx': xs[i], 'dy': ys[j]}
-                        images.add_row(row)
+                        if ktl.waitFor(expr, timeout=20) is False:
+                            lastfile = kpffvc[f'{FVC}LASTFILE'].read()
+                            log.error('No new FVC file found')
+                            log.error(f"  expecting: {nextfile[FVC]}")
+                            log.error(f"  kpffvc.{FVC}LASTFILE = {lastfile}")
+                        else:
+                            lastfile = kpffvc[f'{FVC}LASTFILE'].read()
+                            log.debug(f"Found {lastfile}")
+                            row = {'file': lastfile, 'camera': FVC,
+                                   'dx': xs[i], 'dy': ys[j]}
+                            images.add_row(row)
+
+                # Here's where we wait for the remainder of the TimeOnPosition
+                log.info(f"  Waiting for kpfexpose to be ready")
+                WaitForReady.execute({})
 
                 # Stop Exposure Meter
-                log.info(f"  Waiting for kpfexpose readout to start")
-                WaitForReadout.execute({})
-                log.info(f"  Waiting for ExpMeter DRP")
-                kpf_expmeter['CUR_COUNTS'].wait()
                 log.info(f"  Waiting for ExpMeter to be Ready")
-                EMsuccess = ktl.waitFor('$kpf_expmeter.EXPSTATE == Ready')
+                EMsuccess = ktl.waitFor('$kpf_expmeter.EXPSTATE == Ready', timeout=5)
+                time.sleep(0.5) # Time shim because paranoia
                 if EMsuccess is True:
                     lastfile = kpf_expmeter['FITSFILE'].read()
                 else:
                     lastfile = 'failed'
-                    row = {'file': lastfile, 'camera': 'ExpMeter',
-                           'dx': xs[i], 'dy': ys[j]}
+                log.info(f'  Done.  Lastfile={lastfile}')
+                row = {'file': lastfile, 'camera': 'ExpMeter',
+                       'dx': xs[i], 'dy': ys[j]}
                 images.add_row(row)
+
                 # Retrieve keyword history
                 end = time.time()
                 expmeter_data = {'dx': xs[i], 'dy': ys[j],
@@ -213,23 +256,20 @@ class TestScienceADC(KPFTranslatorFunction):
             if images_file.exists():
                 images_file.unlink()
             images.write(images_file, format='ascii.csv')
-            if 'ExpMeter' in args.get('cameras', '').split(','):
-                if fluxes_file.exists():
-                    fluxes_file.unlink()
-                expmeter_flux.write(fluxes_file, format='ascii.csv')
-
-        if images_file.exists():
-            images_file.unlink()
-        images.write(images_file, format='ascii.csv')
-        if 'ExpMeter' in args.get('cameras', '').split(','):
             if fluxes_file.exists():
                 fluxes_file.unlink()
             expmeter_flux.write(fluxes_file, format='ascii.csv')
 
+        if images_file.exists():
+            images_file.unlink()
+        images.write(images_file, format='ascii.csv')
+        if fluxes_file.exists():
+            fluxes_file.unlink()
+        expmeter_flux.write(fluxes_file, format='ascii.csv')
+
         # Send ADC back to nomimal
         kpffiu['ADCTRACK'].write('On')
 
-
     @classmethod
-    def post_condition(cls, OB, logger, cfg):
+    def post_condition(cls, args, logger, cfg):
         return True
