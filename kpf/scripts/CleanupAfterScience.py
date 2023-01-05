3f944a19bf2f262a444e95a0c8a2892748466dfb
Josh Walawender <joshwalawender@me.com>
2023-01-04 17:57:58 -1000

    add cleanup after science

---
+++ kpf/scripts/CleanupAfterScience.py
@@ -0,0 +1,48 @@
+import os
+from time import sleep
+from packaging import version
+from pathlib import Path
+import yaml
+import numpy as np
+
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
+from . import register_script, obey_scriptrun, check_scriptstop
+from ..fiu.StopTipTilt import StopTipTilt
+
+
+class CleanupAfterScience(KPFTranslatorFunction):
+    '''Script which cleans up at the end of Science OBs.
+    
+    Can be called by `ddoi_script_functions.post_observation_cleanup`.
+    '''
+    @classmethod
+    @obey_scriptrun
+    def pre_condition(cls, OB, logger, cfg):
+        check_input(OB, 'Template_Name', allowed_values=['kpf_sci'])
+        check_input(OB, 'Template_Version', version_check=True, value_min='0.4')
+        return True
+
+    @classmethod
+    @register_script(Path(__file__).name, os.getpid())
+    def perform(cls, OB, logger, cfg):
+        log.info('-------------------------')
+        log.info(f"Running CleanupAfterScience")
+        for key in OB:
+            if key not in ['SEQ_Darks', 'SEQ_Calibrations']:
+                log.debug(f"  {key}: {OB[key]}")
+            else:
+                log.debug(f"  {key}:")
+                for entry in OB[key]:
+                    log.debug(f"    {entry}")
+        log.info('-------------------------')
+
+        # Turn off tip tilt
+        StopTipTilt.execute({})
+
+    @classmethod
+    def post_condition(cls, OB, logger, cfg):
+        return True
