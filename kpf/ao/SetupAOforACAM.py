b3d5c4254e7cf07678ac8f627cdc66d6004aa62a
Josh Walawender <jmwalawender@gmail.com>
2023-01-11 11:39:42 -1000

    Split ACAM setup out to separate script

---
+++ kpf/ao/SetupAOforACAM.py
@@ -0,0 +1,39 @@
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
+from .SetAORotatorManual import SetAORotatorManual
+from .SetAORotator import SetAORotator
+from .TurnHepaOff import TurnHepaOff
+from .SetAODCStoSIM import SetAODCStoSIM
+from .ControlAOHatch import ControlAOHatch
+from .SetAFMtoMirror import SetAFMtoMirror
+from .SendPCUtoHome import SendPCUtoHome
+from .SendPCUtoKPF import SendPCUtoKPF
+from .SetAFStoNGS import SetAFStoNGS
+from .TurnLightSourceOff import TurnLightSourceOff
+
+
+class SetupAOforACAM(KPFTranslatorFunction):
+    '''Set up AO in the safe mode for ACAM operation to assist KPF acquisition
+        1. Set AFM to Mirror
+        2. Set AFS to ngs
+    
+    ARGS: None
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        log.info('Set AFM to Mirror')
+        SetAFMtoMirror.execute({})
+
+        log.info('Set AFS to NGS')
+        SetAFStoNGS.execute({})
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        return True
--- kpf/ao/SetupAOforKPF.py
+++ kpf/ao/SetupAOforKPF.py
@@ -21,11 +21,10 @@ class SetupAOforKPF(KPFTranslatorFunction):
         2. Set AO rotator to 0 deg
         3. Turn off HEPA
         4. Set AO in DCS sim mode
-        5. Set AFm to Mirror
-        6. Set AFS to ngs
-        7. Home PCU <-- to be implemented and tested
-        8. Move PCU to the KPF position <-- to be implemented and tested
-        9. Open AO hatch 
+
+        5. Home PCU <-- to be implemented and tested
+        6. Move PCU to the KPF position <-- to be implemented and tested
+        7. Open AO hatch 
     
     ARGS: None
     '''
@@ -46,12 +45,6 @@ class SetupAOforKPF(KPFTranslatorFunction):
 
         log.info('Set AO in DCS sim mode')
         SetAODCStoSIM.execute({})
-
-        log.info('Set AFM to Mirror')
-        SetAFMtoMirror.execute({})
-
-        log.info('Set AFS to NGS')
-        SetAFStoNGS.execute({})
 
         log.info('Turn K1 AO light source off')
         TurnLightSourceOff.execute({})
