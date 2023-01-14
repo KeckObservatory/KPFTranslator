eca7a228493374b1c601ecdc6bfb1e16da7e7502
Josh Walawender <joshwalawender@me.com>
2023-01-13 19:13:53 -1000

    relax tolerance

--- kpf/guider/SetGuiderFPS.py
+++ kpf/guider/SetGuiderFPS.py
@@ -26,7 +26,7 @@ class SetGuiderFPS(KPFTranslatorFunction):
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
-        fpstol = cfg.get('tolerances', 'guider_fps_tolerance', fallback=0.0001)
+        fpstol = cfg.get('tolerances', 'guider_fps_tolerance', fallback=0.01)
         fpskw = ktl.cache('kpfguide', 'FPS')
         fps = args.get('GuideFPS')
         expr = (f'($kpfguide.FPS >= {fps-fpstol}) and '\
