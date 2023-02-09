afc6a6b8c1b6ef8f7f098694bd21015ed1e2fe85
Josh Walawender <jmwalawender@gmail.com>
2023-02-09 13:54:29 -1000

    No need for a separate log file for this

--- kpf/scripts/CleanupAfterScience.py
+++ kpf/scripts/CleanupAfterScience.py
@@ -18,7 +18,6 @@ class CleanupAfterScience(KPFTranslatorFunction):
         return True
 
     @classmethod
-    @add_script_log(Path(__file__).name.replace(".py", ""))
     def perform(cls, OB, logger, cfg):
         log.info('-------------------------')
         log.info(f"Running {cls.__name__}")
