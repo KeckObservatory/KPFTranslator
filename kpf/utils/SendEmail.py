f37797ae8d3cd95578affd895a3503df32aa4837
Josh Walawender <jmwalawender@gmail.com>
2023-03-03 10:14:40 -1000

    Fix email send failures

--- kpf/utils/SendEmail.py
+++ kpf/utils/SendEmail.py
@@ -2,31 +2,29 @@ import smtplib
 from email.mime.text import MIMEText
 
 from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
-from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
-                FailedToReachDestination, check_input)
+from .. import log, check_input
+
 
 ##-----------------------------------------------------------------------------
-## SetObserverFromSchedule
+## SendEmail
 ##-----------------------------------------------------------------------------
 class SendEmail(KPFTranslatorFunction):
     '''Send an email
     '''
     @classmethod
     def pre_condition(cls, args, logger, cfg):
-        check_input(args, 'Message')
-        check_input(args, 'Subject')
         return True
 
     @classmethod
     def perform(cls, args, logger, cfg):
-        msg = MIMEText(args.get('Message'))
-        msg['Subject'] = args.get('Subject', 'KPF Alert')
+        msg = MIMEText(args.get('Message', 'Test email. Please ignore.'))
         msg['To'] = args.get('To', 'kpf_info@keck.hawaii.edu')
         msg['From'] = args.get('From', 'kpf_info@keck.hawaii.edu')
+        msg['Subject'] = args.get('Subject', 'KPF Alert')
         log.warning(f"Sending email, To {msg['To']}")
         log.warning(f"Sending email, Subject {msg['Subject']}")
         log.warning(f"{args.get('message')}")
-        s = smtplib.SMTP('localhost')
+        s = smtplib.SMTP('relay.keck.hawaii.edu')
         s.send_message(msg)
         s.quit()
 
