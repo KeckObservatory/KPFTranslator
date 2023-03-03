e01ea9c4d6f876e9e9c26f8be2dcd7763deab9ff
Josh Walawender <jmwalawender@gmail.com>
2023-03-03 12:56:20 -1000

    Trigger green mini fill

---
+++ kpf/utils/TriggerGreenMiniFill.py
@@ -0,0 +1,43 @@
+import time
+
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
+from .SendEmail import SendEmail
+
+
+##-------------------------------------------------------------------------
+## TriggerGreenMiniFill
+##-------------------------------------------------------------------------
+class TriggerGreenMiniFill(KPFTranslatorFunction):
+    '''I really hope this is not necessary in the long term.
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        kpffill = ktl.cache('kpffill')
+        if kpffill['GREENFILLIP'].read() == 'True':
+            raise FailedPreCondition('Green fill already in progress')
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        kpffill = ktl.cache('kpffill')
+        log.warning(f'Starting green mini fill')
+        kpffill['GREENSTART'].write(1)
+        sleep_time = 180
+        log.debug(f'Sleeping {sleep_time:.0f} s')
+        log.warning(f'Stopping green mini fill')
+        kpffill['GREENSTOP'].write(1)
+        time.sleep(5)
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        kpffill = ktl.cache('kpffill')
+        if kpffill['GREENFILLIP'].read() == 'True':
+            msg = 'Green still in progress, should be stopped!'
+            SendEmail.execute({'Subject': 'TriggerGreenMiniFill Failed',
+                               'Message': f'{msg}'})
+            raise FailedPostCondition(msg)
+        return True
