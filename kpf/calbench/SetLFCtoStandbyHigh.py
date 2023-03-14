5ceffb69506a27647a5a89fc8103b953879e2115
Josh Walawender <joshwalawender@me.com>
2023-03-14 11:38:45 -1000

    Draft of LFC controls

---
+++ kpf/calbench/SetLFCtoStandbyHigh.py
@@ -0,0 +1,34 @@
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from .. import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                FailedToReachDestination, check_input)
+
+
+class SetLFCtoStandbyHigh(KPFTranslatorFunction):
+    '''
+
+    ARGS:
+    =====
+    None
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        lfc_mode = ktl.cache('kpfcal', 'OPERATIONMODE')
+        lfc_mode.write('StandbyHigh')
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        '''Verifies that kpfmon shows no errors.
+        '''
+        kpfmon = ktl.cache('kpfmon')
+        expr = f"($kpfmon.LFCSTA == 'OK')"
+        timeout = cfg.get('times', 'LFC_startup_time', fallback=60)
+        success = ktl.waitFor(expr, timeout=timeout)
+        if success is not True:
+            raise FailedPostCondition('kpfmon.LFCSTA is not OK')
+        return success
