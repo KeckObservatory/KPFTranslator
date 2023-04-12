14b3d570b34f2d70f30d898074eb2c8a95460c8c
Josh Walawender <joshwalawender@me.com>
2023-04-12 11:52:31 -1000

    Add script to set simulcal/slewcal source

---
+++ kpf/utils/SetSimulCalSource.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+
+import ktl
+
+from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                 FailedToReachDestination, check_input)
+
+
+##-------------------------------------------------------------------------
+## SetSimulCalSource
+##-------------------------------------------------------------------------
+class SetSimulCalSource(KPFTranslatorFunction):
+    '''Set the slew can and simultaneous calibration source.
+
+    Valid names: EtalonFiber, U_gold, U_daily, Th_daily, Th_gold, LFCFiber
+
+    ARGS:
+    =====
+    :calsource: The calibration source to use (must be one of Etalon, LFC,
+                Th_daily, Th_gold, U_daily, U_gold).
+
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        valid_names = ['EtalonFiber', 'U_gold', 'U_daily', 'Th_daily',
+                       'Th_gold', 'LFCFiber']
+        if args.get('calsource', None) not in valid_names:
+            raise FailedPreCondition(f"calsource '{calsource}' must be one of {valid_names}")
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        log.info(f"Setting simul cal / slew cal source")
+        calsource = args.get('calsource')
+        kpfconfig = ktl.cache('kpfconfig')
+        slew_cal_file = Path(f'/kroot/rel/default/data/obs/kpf/SlewCal_{calsource}.yaml')
+        if slew_cal_file.exists() is False:
+            raise KPFException(f'The slew cal file for {calsource} does not exist')
+        else:
+            log.info(f'Writing kpfconfig.SIMULCALSOURCE = {calsource}')
+            kpfconfig['SIMULCALSOURCE'].write(calsource)
+            log.info(f'Writing kpfconfig.SLEWCALFILE = {slew_cal_file}')
+            kpfconfig['SLEWCALFILE'].write(f"{slew_cal_file}")
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def add_cmdline_args(cls, parser, cfg=None):
+        '''The arguments to add to the command line interface.
+        '''
+        from collections import OrderedDict
+        args_to_add = OrderedDict()
+        args_to_add['calsource'] = {'type': str,
+                                    'help': 'Which lamp to use for simultaneous calibration and slew cals.'}
+
+        parser = cls._add_args(parser, args_to_add, print_only=False)
+        return super().add_cmdline_args(parser, cfg)
