e66db2e313851e958886156e33ae776489ab7a34
Josh Walawender <joshwalawender@me.com>
2023-04-15 07:27:05 -1000

    need this import on failure

--- kpf/calbench/TakeIntensityReading.py
+++ kpf/calbench/TakeIntensityReading.py
@@ -4,6 +4,7 @@ import ktl
 from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
 from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
                  FailedToReachDestination, check_input)
+from kpf.utils.SendEmail import SendEmail
 
 
 class TakeIntensityReading(KPFTranslatorFunction):
