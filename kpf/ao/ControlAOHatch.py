161751632e5cab7788a1c7f1232168503bcd5ab7
Josh Walawender <joshwalawender@me.com>
2023-04-21 14:54:28 -1000

    Use argparse directly to set cmd line args

--- kpf/ao/ControlAOHatch.py
+++ kpf/ao/ControlAOHatch.py
@@ -37,10 +37,7 @@ class ControlAOHatch(KPFTranslatorFunction):
     def add_cmdline_args(cls, parser, cfg=None):
         '''The arguments to add to the command line interface.
         '''
-        from collections import OrderedDict
-        args_to_add = OrderedDict()
-        args_to_add['destination'] = {'type': str,
-                                'help': 'Desired hatch position: "open" or "close"'}
-
-        parser = cls._add_args(parser, args_to_add, print_only=False)
+        parser.add_argument('destination', type=str,
+                            help='Desired hatch position: "open" or "close"')
         return super().add_cmdline_args(parser, cfg)
+
