05b2fcd25790d5fdf1ab32c13d37979ebd67d896
Josh Walawender <joshwalawender@me.com>
2023-04-26 13:51:30 -1000

    fix

--- kpf/scripts/ConfigureForCalibrations.py
+++ kpf/scripts/ConfigureForCalibrations.py
@@ -54,7 +54,7 @@ class ConfigureForCalibrations(KPFTranslatorFunction):
         sequence = OB.get('SEQ_Calibrations')
         lamps = set([x['CalSource'] for x in sequence if x['CalSource'] != 'Home'])
         for lamp in lamps:
-            if IsCalSourceEnabled.execute(OB) == True:
+            if IsCalSourceEnabled.execute({'CalSource': lamp}) == True:
                 if lamp in ['Th_daily', 'Th_gold', 'U_daily', 'U_gold',
                             'BrdbandFiber', 'WideFlat']:
                     CalLampPower.execute({'lamp': lamp, 'power': 'on'})
