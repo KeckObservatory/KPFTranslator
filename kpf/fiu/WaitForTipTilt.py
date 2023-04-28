0223fa48056866acd9e2753d577d8ab8cc2e4a8d
Josh Walawender <joshwalawender@me.com>
2023-04-28 05:19:49 -1000

    fix

--- kpf/fiu/WaitForTipTilt.py
+++ kpf/fiu/WaitForTipTilt.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 import ktl
 
-from ddoitranslatormodule.KPFTranslatorFunction import KPFTranslatorFunction
+from kpf.KPFTranslatorFunction import KPFTranslatorFunction
 from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
                  FailedToReachDestination, check_input, LostTipTiltStar)
 
