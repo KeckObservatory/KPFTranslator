7cf84d56ca68aa52318f4596436beecf7e748003
Josh Walawender <joshwalawender@me.com>
2023-07-07 13:29:19 -1000

    Draft DetermineReadMode

---
+++ kpf/spectrograph/DetermineReadMode.py
@@ -0,0 +1,39 @@
+import time
+import ktl
+
+from kpf.KPFTranslatorFunction import KPFTranslatorFunction
+from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                 FailedToReachDestination, check_input)
+
+
+class DetermineReadMode(KPFTranslatorFunction):
+    '''
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        pass
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        kpgreen = ktl.cache('kpfgreen')
+        kpfred = ktl.cache('kpfred')
+        green_ACF = kpfgreen['ACF'].read()
+        red_ACF = kpfred['ACF'].read()
+
+        green_normal_file = cfg.get('acf_files', 'green_normal')
+        green_fast_file = cfg.get('acf_files', 'green_fast')
+        red_normal_file = cfg.get('acf_files', 'red_normal')
+        red_fast_file = cfg.get('acf_files', 'red_fast')
+
+        if (green_ACF == green_normal_file) and (red_ACF == red_normal_file):
+            mode = 'normal'
+        elif (green_ACF == green_fast_file) and (red_ACF == red_fast_file):
+            mode = 'fast'
+        else:
+            mode = 'unknown'
+
+        return mode
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        pass
