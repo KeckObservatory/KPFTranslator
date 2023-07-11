7e06778cd9db85b7627672d94eba546ea6c26abe
Josh Walawender <joshwalawender@me.com>
2023-07-11 12:42:22 -1000

    Add scripts to set read mode

---
+++ kpf/spectrograph/SetReadModeNormal.py
@@ -0,0 +1,33 @@
+import time
+import ktl
+
+from kpf.KPFTranslatorFunction import KPFTranslatorFunction
+from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
+                 FailedToReachDestination, check_input)
+from kpf.spectrograph.QueryFastReadMode import QueryFastReadMode
+
+
+class SetReadModeNormal(KPFTranslatorFunction):
+    '''
+
+    ARGS:
+    None
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        pass
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        kpfgreen = ktl.cache('kpfgreen')
+        kpfred = ktl.cache('kpfred')
+        green_normal_file = cfg.get('acf_files', 'green_normal')
+        red_normal_file = cfg.get('acf_files', 'red_normal')
+        kpfgreen['ACF'].write(green_normal_file)
+        kpfred['ACF'].write(red_normal_file)
+        time.sleep(2)
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        if QueryFastReadMode.execute({}) != False:
+            raise FailedPostCondition(f"Read mode change failed")
