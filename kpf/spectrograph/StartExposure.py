b25d4cb35bed838a3e36b8da098438f84801482c
Josh Walawender <jmwalawender@gmail.com>
2023-09-19 15:54:41 -1000

    Draft of restart exposure on start state error

--- kpf/spectrograph/StartExposure.py
+++ kpf/spectrograph/StartExposure.py
@@ -29,15 +29,28 @@ class StartExposure(KPFTranslatorFunction):
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
-        expr = f"(kpfexpose.EXPOSE != Start or kpfmon.CAMSTATESTA == ERROR)"
-        timeout = 7 # This is to allow 5s for kpfmon.CAMSTATESTA to work
-        error_or_exposure_inprogress = ktl.waitFor(expr, timeout=timeout)
-        if error_or_exposure_inprogress == True:
-            # We didn't time out, so which state is it?
-            kpfmon = ktl.cache('kpfmon')
-            camstatesta = kpfmon['CAMSTATESTA'].read()
-            if camstatesta == 'ERROR':
-                log.error('kpfexpose.CAMSTATESTA is ERROR')
+        expr = f"(kpfexpose.EXPOSE != Start)"
+        kpfexpose = ktl.cache('kpfexpose')
+        trig_targ = kpfexpose['TRIG_TARG'].read().split(',')
+        if 'Green' in trig_targ:
+            expr += ' and ($kpfgreen.EXPSTATE != Start)'
+        if 'Red' in trig_targ:
+            expr += ' and ($kpfred.EXPSTATE != Start)'
+        if 'Ca_HK' in trig_targ:
+            expr += ' and ($kpf_hk.EXPSTATE != Start)'
+        timeout = 5
+        left_start_state = ktl.waitFor(expr, timeout=timeout)
+        if left_start_state is not True:
+            log.error(f'We are still in start state after {timeout} s')
+            if 'Green' in trig_targ:
+                green_expstate = ktl.cache('kpfgreen', 'EXPSTATE')
+                log.debug(f'kpfgreen.EXPSTATE = {green_expstate.read()}')
+            if 'Red' in trig_targ:
+                red_expstate = ktl.cache('kpfred', 'EXPSTATE')
+                log.debug(f'kpfred.EXPSTATE = {red_expstate.read()}')
+            if 'Ca_HK' in trig_targ:
+                cahk_expstate = ktl.cache('kpf_hk', 'EXPSTATE')
+                log.debug(f'kpf_hk.EXPSTATE = {cahk_expstate.read()}')
                 RecoverDetectors.execute({})
                 # Now we want to abort the current exposure and start again
                 log.warning('Stopping current exposure (with read out)')
@@ -45,11 +58,30 @@ class StartExposure(KPFTranslatorFunction):
                 expose.write('End')
                 WaitForReady.execute({})
                 StartExposure.execute(args)
-            else:
-                # We must have transitioned properly
-                expose = ktl.cache('kpfexpose', 'EXPOSE')
-                log.debug(f'Post condition succeeded: kpfmon.CAMSTATESTA={camstatesta}, kpfexpose.EXPOSE={expose.read()}')
-        else:
-            # We timed out, not sure what is going on
-            msg = f"Neither kpfmon.CAMSTATESTA nor kpfexpose.EXPOSE transitioned as expected"
-            raise KPFException(msg)
+
+#     @classmethod
+#     def post_condition(cls, args, logger, cfg):
+#         expr = f"(kpfexpose.EXPOSE != Start or kpfmon.CAMSTATESTA == ERROR)"
+#         timeout = 7 # This is to allow 5s for kpfmon.CAMSTATESTA to work
+#         error_or_exposure_inprogress = ktl.waitFor(expr, timeout=timeout)
+#         if error_or_exposure_inprogress == True:
+#             # We didn't time out, so which state is it?
+#             kpfmon = ktl.cache('kpfmon')
+#             camstatesta = kpfmon['CAMSTATESTA'].read()
+#             if camstatesta == 'ERROR':
+#                 log.error('kpfexpose.CAMSTATESTA is ERROR')
+#                 RecoverDetectors.execute({})
+#                 # Now we want to abort the current exposure and start again
+#                 log.warning('Stopping current exposure (with read out)')
+#                 expose = ktl.cache('kpfexpose', 'EXPOSE')
+#                 expose.write('End')
+#                 WaitForReady.execute({})
+#                 StartExposure.execute(args)
+#             else:
+#                 # We must have transitioned properly
+#                 expose = ktl.cache('kpfexpose', 'EXPOSE')
+#                 log.debug(f'Post condition succeeded: kpfmon.CAMSTATESTA={camstatesta}, kpfexpose.EXPOSE={expose.read()}')
+#         else:
+#             # We timed out, not sure what is going on
+#             msg = f"Neither kpfmon.CAMSTATESTA nor kpfexpose.EXPOSE transitioned as expected"
+#             raise KPFException(msg)
