013cefd56b2257ead43539b051ff6b5414de16c1
Josh Walawender <joshwalawender@me.com>
2024-07-25 10:55:01 -1000

    Send email on mode changes

--- kpf/spectrograph/SetReadModeFast.py
+++ kpf/spectrograph/SetReadModeFast.py
@@ -5,6 +5,7 @@ from kpf.KPFTranslatorFunction import KPFTranslatorFunction
 from kpf import (log, KPFException, FailedPreCondition, FailedPostCondition,
                  FailedToReachDestination, check_input)
 from kpf.spectrograph.QueryReadMode import QueryReadMode
+from kpf.utils.SendEmail import SendEmail
 
 
 class SetReadModeFast(KPFTranslatorFunction):
@@ -23,6 +24,14 @@ class SetReadModeFast(KPFTranslatorFunction):
     def perform(cls, args, logger, cfg):
         green_mode, red_mode = QueryReadMode.execute({})
         if green_mode != 'fast':
+            msg = f'Setting Green CCD read mode fast'
+            log.info(msg)
+            # Email to kpf_info
+            try:
+                SendEmail.execute({'Subject': msg, 'Message': msg})
+            except Exception as email_err:
+                log.error(f'Sending email failed')
+                log.error(email_err)
             kpfgreen = ktl.cache('kpfgreen')
             green_fast_file = cfg.get('acf_files', 'green_fast')
             green_ACFFILE = Path(kpfgreen['ACFFILE'].read()).stem
@@ -30,6 +39,14 @@ class SetReadModeFast(KPFTranslatorFunction):
                 kpfgreen['ACF'].write(green_fast_file)
             time.sleep(1)
         if red_mode != 'fast':
+            msg = f'Setting Red CCD read mode fast'
+            log.info(msg)
+            # Email to kpf_info
+            try:
+                SendEmail.execute({'Subject': msg, 'Message': msg})
+            except Exception as email_err:
+                log.error(f'Sending email failed')
+                log.error(email_err)
             kpfred = ktl.cache('kpfred')
             red_fast_file = cfg.get('acf_files', 'red_fast')
             red_ACFFILE = Path(kpfred['ACFFILE'].read()).stem
--- kpf/spectrograph/SetReadModeNormal.py
+++ kpf/spectrograph/SetReadModeNormal.py
@@ -23,6 +23,14 @@ class SetReadModeNormal(KPFTranslatorFunction):
     def perform(cls, args, logger, cfg):
         green_mode, red_mode = QueryReadMode.execute({})
         if green_mode != 'normal':
+            msg = f'Setting Green CCD read mode normal'
+            log.info(msg)
+            # Email to kpf_info
+            try:
+                SendEmail.execute({'Subject': msg, 'Message': msg})
+            except Exception as email_err:
+                log.error(f'Sending email failed')
+                log.error(email_err)
             kpfgreen = ktl.cache('kpfgreen')
             green_normal_file = cfg.get('acf_files', 'green_normal')
             green_ACFFILE = Path(kpfgreen['ACFFILE'].read()).stem
@@ -30,6 +38,14 @@ class SetReadModeNormal(KPFTranslatorFunction):
                 kpfgreen['ACF'].write(green_normal_file)
             time.sleep(1)
         if red_mode != 'normal':
+            msg = f'Setting Red CCD read mode normal'
+            log.info(msg)
+            # Email to kpf_info
+            try:
+                SendEmail.execute({'Subject': msg, 'Message': msg})
+            except Exception as email_err:
+                log.error(f'Sending email failed')
+                log.error(email_err)
             kpfred = ktl.cache('kpfred')
             red_normal_file = cfg.get('acf_files', 'red_normal')
             red_ACFFILE = Path(kpfred['ACFFILE'].read()).stem
