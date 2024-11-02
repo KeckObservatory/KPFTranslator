a51ebb54820e5a49e03e7cd8afa50027a78e831b
Josh Walawender <jmwalawender@gmail.com>
2024-11-01 15:20:08 -1000

    Have setup file for testing

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='KPFTranslator',
+    version='1.4.2dev',    
+    description='KPFTranslator',
+    url='https://github.com/KeckObservatory/KPFTranslator',
+    author='Josh Walawender',
+    author_email='jwalawender@keck.hawaii.edu',
+    license='BSD 2-clause',
+#     packages=find_namespace_packages(where='kpf', include=['kpf']),
+    packages=['kpf',
+              'kpf.ObservingBlocks',
+              'kpf.utils',
+              ],
+    package_data={"kpf.ObservingBlocks": ["*.yaml"]},
+    include_package_data=True,
+    install_requires=['numpy'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
