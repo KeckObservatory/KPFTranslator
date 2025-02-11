35152fb5400315ee67daf650d0dbb8acfa342251
Josh Walawender <jmwalawender@gmail.com>
2025-02-10 19:52:00 -1000

    Initial draft without comment field

--- kpf/utils/CollectExecutionHistory.py
+++ kpf/utils/CollectExecutionHistory.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import datetime
 import json
@@ -26,13 +27,13 @@ class CollectExecutionHistory(KPFFunction):
         OBid = args.get('OBid', None)
         params = {}
         params["id"] = f"{OBid}"
-        if OBid is None:
-            return
+#         if OBid is None:
+#             return
         log.info(f"Running {cls.__name__}")
         SCRIPTPID_hist = keygrabber.retrieve({'kpfconfig': ['SCRIPTPID']},
             begin=time.mktime(datetime.datetime.now().timetuple()))
         log.debug('Getting start time of script')
-        begin = datetime.datetime.fromtimestamp(SCRIPTPID_history[0]['time'])
+        begin = SCRIPTPID_hist[0]['time']
 
         log.debug('Getting OBSERVER history')
         observer_hist = keygrabber.retrieve({'kpfexpose': ['OBSERVER']}, begin=begin)
@@ -41,6 +42,7 @@ class CollectExecutionHistory(KPFFunction):
         log.debug('Getting STARTTIME history')
         start_times = []
         start_hist = keygrabber.retrieve({'kpfexpose': ['STARTTIME']}, begin=begin)
+        start_hist.pop(0)
         tzconversion = datetime.timedelta(hours=10)
         for s in start_hist:
             d = datetime.datetime.fromtimestamp(s['time'])
@@ -49,13 +51,34 @@ class CollectExecutionHistory(KPFFunction):
         params["exposure_start_times"] = start_times
 
         log.debug('Getting ELAPSED history')
-        elapsed_hist = keygrabber.retrieve({'kpfexpose': ['ELAPSED']}, begin=begin)
-#         exp_times = []
-#         for s in elapsed_hist:
-#             d = datetime.datetime.fromtimestamp(s['time'])
-#             ut = d + tzconversion
-#             exp_times.append(ut.isoformat())
-#         params["exposure_times"] = exp_times
+        elapsed_hist = keygrabber.retrieve({'kpfexpose': ['ELAPSED', 'EXPOSE']}, begin=begin)
+        exp_times = []
+        became_ready = False
+        readout = False
+        last_elapsed = 0
+        for s in elapsed_hist:
+            if (s['keyword'] == 'EXPOSE') and (s['ascvalue'] == 'Ready'):
+                became_ready = True
+#                 print('Became ready')
+            elif (s['keyword'] == 'EXPOSE') and (s['ascvalue'] == 'Readout') and became_ready:
+                readout = True
+#                 print('readout is true')
+#                 print(f'Grabbing elapsed: {last_elapsed}')
+                exp_times.append(last_elapsed)
+            elif (s['keyword'] == 'EXPOSE') and (s['ascvalue'] != 'Readout'):
+                readout = False
+#                 print('readout is false')
+            if (s['keyword'] == 'ELAPSED'):
+                last_elapsed = float(s["binvalue"])
+        params["exposure_times"] = exp_times
+
+        if len(params["exposure_times"]) != len(params["exposure_start_times"]):
+            print(len(params["exposure_start_times"]), params["exposure_start_times"])
+            print(len(params["exposure_times"]), params["exposure_times"])
+            print()
+            print(f'Mismatch in start times and exposure times')
+            sys.exit(0)
+            raise KPFException(f'Mismatch in start times and exposure times')
 
 #         log.debug('Getting OBSERVERCOMMENT history')
 #         comment_hist = keygrabber.retrieve({'kpfconfig': ['OBSERVERCOMMENT']}, begin=begin)
