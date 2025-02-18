dd8ac87763e005d7e1adb7155693e542427373d0
Josh Walawender <joshwalawender@me.com>
2025-02-18 13:19:38 -1000

    Adjust size of OB details window

--- kpf/OB_GUI/KPF_OB_GUI.py
+++ kpf/OB_GUI/KPF_OB_GUI.py
@@ -124,7 +124,7 @@ class ScrollMessageBox(QtWidgets.QMessageBox):
         contents_label.setFont(QtGui.QFont('Courier New', 11))
         lay.addWidget(contents_label)
         self.layout().addWidget(scroll, 0, 0, 1, self.layout().columnCount())
-        self.setStyleSheet("QScrollArea{min-width:300 px; min-height: 700px}")
+        self.setStyleSheet("QScrollArea{min-width:350 px; min-height: 600px;}")
 
 
 ##-------------------------------------------------------------------------
