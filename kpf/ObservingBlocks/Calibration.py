f2e5b4aed4b93bc79f129205ccb00e884c49cbea
Josh Walawender <joshwalawender@me.com>
2025-03-10 15:18:47 -1000

    Update comments and validation

--- kpf/ObservingBlocks/Calibration.py
+++ kpf/ObservingBlocks/Calibration.py
@@ -11,12 +11,16 @@ class Calibration(BaseOBComponent):
             properties = yaml.safe_load(f.read())
         super().__init__('Calibration', '2.0', properties=properties)
         self.list_element = True
-        skip_if_dark = ['IntensityMonitor', 'CalND1', 'CalND2',
-                        'OpenScienceShutter', 'OpenSkyShutter',
-                        'TakeSimulCal', 'WideFlatPos', 'ExpMeterMode',
-                        'ExpMeterExpTime', 'ExpMeterBin',
-                        'ExpMeterThreshold']
-        self.pruning_guide = [(self.get('CalSource').lower() in ['dark', 'home'], skip_if_dark),
+        self.calsources = ['Dark', 'Home', 'dark', 'WideFlat', 'BrdbandFiber',
+                           'U_gold', 'U_daily', 'Th_daily', 'Th_gold',
+                           'LFCFiber', 'EtalonFiber',
+                           'SoCal-CalFib', 'SoCal-SciSky']
+        self.skip_if_dark = ['IntensityMonitor', 'CalND1', 'CalND2',
+                             'OpenScienceShutter', 'OpenSkyShutter',
+                             'TakeSimulCal', 'WideFlatPos', 'ExpMeterMode',
+                             'ExpMeterExpTime', 'ExpMeterBin',
+                             'ExpMeterThreshold']
+        self.pruning_guide = [(self.get('CalSource').lower() in ['dark', 'home'], self.skip_if_dark),
                               (self.get('ExpMeterMode') in ['off', 'False', False], ['ExpMeterExpTime']),
                               (self.get('ExpMeterMode') != 'control', ['ExpMeterBin', 'ExpMeterThreshold']),
                               (self.get('TakeSimulCal') == False, ['CalND1', 'CalND2']),
@@ -33,7 +37,35 @@ class Calibration(BaseOBComponent):
             if self.get(p['name']) is None:
                 print(f"ERROR: {p['name']} is undefined, default is {p['defaultvalue']}")
                 valid = False
+        # CalSource is not allowed value
+        if self.get('CalSource') not in self.calsources:
+            print(f"ERROR: CalSource invalid")
+            valid = False
+        # Check if Object is empty
+        if self.get('Object') in ['', None]:
+            print(f"ERROR: Object field is empty")
+            valid = False
         return valid
+
+
+    def add_comment(self, pname):
+        # CalSource is not allowed value
+        if self.get('CalSource') not in self.calsources:
+            if pname in ['CalSource']:
+                return ' # ERROR: CalSource invalid'
+        # Object is empty
+        if self.get('Object') in ['', None]:
+            if pname in ['Object']:
+                return ' # ERROR: Object field is empty'
+        # CalSource is dark
+        if self.get('CalSource').lower() in ['dark', 'home']:
+            if pname in self.skip_if_dark:
+                return ' # Unused: CalSource == Dark'
+        # TakeSimulcal is false
+        if self.get('TakeSimulCal') == False:
+            if pname in ['CalND2']:
+                return ' # Unused: TakeSimulCal == False'
+        return ''
 
 
     def __str__(self):
