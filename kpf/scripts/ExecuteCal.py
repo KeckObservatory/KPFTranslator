842875ef0375bef3727007f943c82babdc5af4d0
Josh Walawender <joshwalawender@me.com>
2025-04-11 19:03:23 -1000

    Update for new SPECFLAT keywords

--- kpf/scripts/ExecuteCal.py
+++ kpf/scripts/ExecuteCal.py
@@ -132,12 +132,14 @@ class ExecuteCal(KPFFunction):
                         HB_MENLOSTA = ktl.cache('kpfmon', 'HB_MENLOSTA')
                         LFCREADYSTA = ktl.cache('kpfmon', 'LFCREADYSTA')
                         WOBBLE = ktl.cache('kpfcal', 'WOBBLE')
-                        SPECFLAT = ktl.cache('kpfcal', 'SPECFLAT')
+                        SPECFLATVIS = ktl.cache('kpfcal', 'SPECFLATVIS')
+                        SPECFLATIR = ktl.cache('kpfcal', 'SPECFLATIR')
                         msg = [f'Failed to set LFC to AstroComb',
                                f'kpfmon.HB_MENLOSTA = {HB_MENLOSTA.read()}',
                                f'kpfmon.LFCREADYSTA = {LFCREADYSTA.read()}',
                                f'kpfcal.WOBBLE = {WOBBLE.read()}',
-                               f'kpfcal.SPECFLAT = {SPECFLAT.read()}']
+                               f'kpfcal.SPECFLATVIS = {SPECFLATVIS.read()}',
+                               f'kpfcal.SPECFLATIR = {SPECFLATIR.read()}']
                         SendEmail.execute({'Subject': 'Failed to set LFC to AstroComb',
                                            'Message': '\n'.join(msg)})
                     except Exception as email_err:
