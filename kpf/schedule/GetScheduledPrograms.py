5fdd7120eb973aa6bfe27558b2b843a2fccd6f52
Josh Walawender <joshwalawender@me.com>
2025-04-15 15:47:42 -1000

    Update schedule query to get more useful set of values

--- kpf/schedule/GetScheduledPrograms.py
+++ kpf/schedule/GetScheduledPrograms.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timedelta
+
 from kpf import log, cfg
 from kpf.exceptions import *
 from kpf.KPFTranslatorFunction import KPFFunction, KPFScript
-from kpf.schedule import getSchedule
+from kpf.schedule import get_semester_dates, getSchedule
 
 
 class GetScheduledPrograms(KPFFunction):
@@ -14,14 +16,30 @@ class GetScheduledPrograms(KPFFunction):
 
     @classmethod
     def perform(cls, args):
-        return [entry['ProjCode'] for entry in getSchedule(**args)]
+        '''Returns list of dicts with the classical and cadence program info.
+        
+        If no args, returns everything remaining on this semester's schedule.
+        If args contains a value for "semester" then that is either interpreted
+        as the semester string (e.g. "2025A") or it can be "current" which
+        instructs the code to use the current semester.
+        '''
+        utnow = datetime.utcnow()
+        semester, semester_start, semester_end = get_semester_dates(utnow)
+        if args.get('semester', None) == 'current':
+            start = semester_start
+        elif int(str(args.get('semester', None))[:4]) > 2000:
+            semester, semester_start, semester_end = get_semester_dates(args.get('semester'))
+            start = semester_start
+        else:
+            # Pull programs from the rest of the semester
+            start = utnow
+        date_str = (start-timedelta(days=1)).strftime('%Y-%m-%d')
+        numdays = (semester_end-start).days + 2
+        all_programs = getSchedule(date=date_str, numdays=numdays)
+        classical_programs = [p for p in all_programs if p['Instrument'] == 'KPF' and p['Semester'] == semester]
+        cadence_programs = [p for p in all_programs if p['Instrument'] == 'KPF-CC' and p['Semester'] == semester]
+        return classical_programs, cadence_programs
 
     @classmethod
     def post_condition(cls, args):
         pass
-
-    @classmethod
-    def add_cmdline_args(cls, parser):
-        parser.add_argument('date', type=str,
-            help="date")
-        return super().add_cmdline_args(parser)
