0aff695bf95c6fc808e6ed090f724ffa82f70f87
Josh Walawender <joshwalawender@me.com>
2025-05-02 12:39:20 -1000

    Use kpfconfig keywords instead of dcs

--- kpf/utils/SetTargetInfo.py
+++ kpf/utils/SetTargetInfo.py
@@ -24,30 +24,21 @@ class SetTargetInfo(KPFTranslatorFunction):
         log.info(f"Setting target parameters")
         kpfconfig = ktl.cache('kpfconfig')
         kpf_expmeter = ktl.cache('kpf_expmeter')
-        dcs = ktl.cache('dcs1')
         kpfconfig['TARGET_NAME'].write(OB.get('TargetName', ''))
         kpfconfig['TARGET_GAIA'].write(OB.get('GaiaID', ''))
         kpfconfig['TARGET_2MASS'].write(OB.get('2MASSID', ''))
         kpfconfig['TARGET_GMAG'].write(OB.get('Gmag', ''))
         kpfconfig['TARGET_JMAG'].write(OB.get('Jmag', ''))
+        TARGPLAX = OB.get('Parallax', 0)
+        kpfconfig['TARGET_PAR'].write(f"{TARGPLAX:.3f}")
+        TARGRADV = OB.get('RadialVelocity', 0)
+        kpfconfig['TARGET_RADV'].write(f"{TARGRADV:.3f}")
 
         TARGET_TEFF = OB.get('Teff', 45000)
         try:
             kpf_expmeter['TARGET_TEFF'].write(float(TARGET_TEFF))
         except:
             log.warning(f"Unable to set kpf_expmeter.TARGET_TEFF to {TARGET_TEFF} ({type(TARGET_TEFF)})")
-
-        TARGPLAX = OB.get('Parallax', 0)
-        try:
-            dcs['TARGPLAX'].write(float(TARGPLAX))
-        except:
-            log.warning(f"Unable to set dcs.TARGPLAX to {TARGPLAX} ({type(TARGPLAX)})")
-
-        TARGRADV = OB.get('RadialVelocity', 0)
-        try:
-            dcs['TARGRADV'].write(float(TARGRADV))
-        except:
-            log.warning(f"Unable to set dcs.TARGRADV to {TARGRADV} ({type(TARGRADV)})")
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
