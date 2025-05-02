d09d3731e28363571d5786cad11028edaff1995f
Josh Walawender <joshwalawender@me.com>
2025-05-02 12:41:05 -1000

    Use kpfconfig instead of dcs to track parallax and radv

--- kpf/scripts/SetTargetInfo.py
+++ kpf/scripts/SetTargetInfo.py
@@ -11,15 +11,6 @@ from kpf.KPFTranslatorFunction import KPFFunction, KPFScript
 class SetTargetInfo(KPFScript):
     '''Set the target info keywords based on the target information in the OB.
 
-    Note that there is a unit mismatch problem in this code, but it is accounted
-    for elsewhere. The dcs units for parallax are arcsec, but we are writing a
-    value in milliarcsec to the keyword. We found this in early 2025 and have
-    decided to leave the behavior the same and just make sure that the FITS
-    header comment is updated to reflect the units of milliarcsec for the value.
-    This works because dcs is being used here only as a carrier of this
-    information and both the source (the OB) and the destination (FITS header)
-    are now in agreement on units.
-
     ### ARGS
     **OB**: (`dict`) A fully specified observing block (OB) or at least the
             target components of an OB.
@@ -43,30 +34,16 @@ class SetTargetInfo(KPFScript):
         kpfconfig['TARGET_2MASS'].write(targ.get('2MASSID', ''))
         kpfconfig['TARGET_GMAG'].write(targ.get('Gmag', ''))
         kpfconfig['TARGET_JMAG'].write(targ.get('Jmag', ''))
+        TARGPLAX = OB.get('Parallax', 0)
+        kpfconfig['TARGET_PAR'].write(f"{TARGPLAX:.3f}")
+        TARGRADV = OB.get('RadialVelocity', 0)
+        kpfconfig['TARGET_RADV'].write(f"{TARGRADV:.3f}")
 
         TARGET_TEFF = targ.get('Teff', 45000)
         try:
             kpf_expmeter['TARGET_TEFF'].write(float(TARGET_TEFF))
         except:
             log.warning(f"Unable to set kpf_expmeter.TARGET_TEFF to {TARGET_TEFF} ({type(TARGET_TEFF)})")
-
-        # Handle DCS target values
-        dcs = ktl.cache('dcs1')
-        if dcs['INSTRUME'].read() != 'KPF':
-            log.debug('Instrument is not KPF. Not setting DCS values.')
-            return
-
-        TARGPLAX = targ.get('Parallax', 0)
-        try:
-            dcs['TARGPLAX'].write(float(TARGPLAX))
-        except:
-            log.warning(f"Unable to set dcs.TARGPLAX to {TARGPLAX} ({type(TARGPLAX)})")
-
-        TARGRADV = targ.get('RadialVelocity', 0)
-        try:
-            dcs['TARGRADV'].write(float(TARGRADV))
-        except:
-            log.warning(f"Unable to set dcs.TARGRADV to {TARGRADV} ({type(TARGRADV)})")
 
     @classmethod
     def post_condition(cls, args, OB=None):
