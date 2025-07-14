0bf27faca09861437373228fe1081201e7d84c55
Josh Walawender <jmwalawender@gmail.com>
2025-07-13 19:48:00 -1000

    fix

--- kpf/socal/CheckSoCalEnabled.py
+++ kpf/socal/CheckSoCalEnabled.py
@@ -18,16 +18,16 @@ class CheckSoCalEnabled(KPFFunction):
     @classmethod
     def perform(cls, args):
         CAN_OPEN = ktl.cache('kpfsocal', 'CAN_OPEN')
-        if CAN_OPEN.read() == 'Yes':
-            print('CAN_OPEN is "Yes"')
+        if CAN_OPEN.read(binary=True) == True:
+            print('CAN_OPEN is True')
         else:
-            print('WARNING: CAN_OPEN is not "Yes"')
+            print('WARNING: CAN_OPEN is not True')
             try:
-                msg = [f'KPF SoCal CAN_OPEN is not "Yes"',
+                msg = [f'KPF SoCal CAN_OPEN is not True',
                        f'',
                        f'SoCal is disabled.',
                        ]
-                SendEmail.execute({'Subject': 'KPF SoCal CAN_OPEN is not "Yes"',
+                SendEmail.execute({'Subject': 'KPF SoCal CAN_OPEN is not True',
                                    'Message': '\n'.join(msg)})
             except Exception as email_err:
                 log.error(f'Sending email failed')
