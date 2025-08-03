bf3a821776ce538d934b0f0f88e3052c07996ad0
Josh Walawender <jmwalawender@gmail.com>
2025-08-02 17:26:20 -1000

    Update etalon file

--- kpf/calbench/PredictNDFilters.py
+++ kpf/calbench/PredictNDFilters.py
@@ -266,7 +266,7 @@ class PredictNDFilters(KPFFunction):
         # reference calibration file to scale up/down
         #cal_file = 'KP.20240529.80736.43_L1.fits' # reference etalon L1 file
         data_dir = Path(__file__).parent.parent.parent / 'data'
-        cal_file = data_dir / 'KP.20250501.79854.92_L1.fits'
+        cal_file = data_dir / 'KP.20250802.80643.77_L1.fits'
 
         # Filter wheel populations for both wheels
 #         od_arr_scical = [0.1, 0.3, 0.5, 0.8, 1.0, 4.0]
