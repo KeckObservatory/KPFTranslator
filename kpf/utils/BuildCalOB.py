9e58d3e1880030ee6405c98db7050ea1ce5e454c
Josh Walawender <joshwalawender@me.com>
2025-08-28 14:46:16 -1000

    Add docstring for BuildCalOB

--- kpf/utils/BuildCalOB.py
+++ kpf/utils/BuildCalOB.py
@@ -13,7 +13,46 @@ from kpf.scripts.RunOB import RunOB
 ##-------------------------------------------------------------------------
 class BuildCalOB(KPFFunction):
     '''From a set of standard prescriptions, build the described calibration OB.
+    Given a list of input strings, parse them in to variations of the standard
+    calibrations and create an OB for them.
 
+    The input string format is `[CalSource],[Object],[nExp],[keywords]`. The
+    first 3 entries are required while the keywords section is optional. Any
+    unspecified values default to match the same CalSource entry in the example
+    OB file in this repo at `kpf/ObservingBlocks/exampleOBs/Calibration.yaml`.
+
+    For example, and input of `EtalonFiber,slewcal,5` will generate an OB which
+    takes 5 etalon frames with the object name of "slewcal". No keywords are
+    provided, so all other elements of the OB are the default values (e.g. the
+    exposure time and ND filters).
+
+    The keywords can be used to modify those default values in the OB. For
+    example, if the input is:
+    `[CalSource],[Object],[nExp],[keywords],IntensityMonitor=True`
+    then the resulting OB will have the `IntensityMonitor` value set to True
+    rather than the default of False and
+    `[CalSource],[Object],[nExp],[keywords],ExpTime=30`
+    will result in an OB with 30 second exposure times instead of the default
+    of 40 seconds.
+
+    Multiple inputs can be given to create multi-calibration OBs, either as a
+    list in python or on the command line. For example:
+    `kpfdo BuildCalOB Dark,autocal-dark,1,ExpTime=1200 EtalonFiber,autocal-etalon-all-midday,3,IntensityMonitor=True BrdbandFiber,autocal-flat-all,95,IntensityMonitor=True`
+    will generate an OB with three calibrations: a 1200 second dark, 3 Etalon
+    frames, and 95 flat field frames.
+
+    Args:
+        calinputs (list of strings): Input strings as described above.
+        estimate (bool): Generate the OB, run the `EstimateOBDuration` tool,
+                         and print the results to screen.
+        save (string): Save resulting OB to the specified file.
+        overwrite (bool): Overwrite output file if it exists?
+        execute (bool): Execute the resulting OB?
+
+    Functions Called:
+
+    - `EstimateOBDuration`
+    - `RunOB`
     '''
     @classmethod
     def pre_condition(cls, args):
