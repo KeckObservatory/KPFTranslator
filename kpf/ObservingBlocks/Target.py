3ee3773e1730e7cce6c32857b117f1070920a15c
Josh Walawender <joshwalawender@me.com>
2025-12-01 13:52:39 -1000

    Put proper motions in star list output

--- kpf/ObservingBlocks/Target.py
+++ kpf/ObservingBlocks/Target.py
@@ -115,6 +115,8 @@ class Target(BaseOBComponent):
             out += f" 2000"
         else:
             out += f" {self.Equinox}"
+        if abs(self.PMRA.value) > 0 or abs(self.PMDEC.value) > 0:
+               out += f" pmra={self.PMRA:.4f} pmdec={self.PMDEC:.3f}"
         if abs(self.DRA.value) > 0 or abs(self.DDEC.value) > 0:
                out += f" dra={self.DRA:.3f} ddec={self.DDEC:.3f}"
 #         out += f" # Gmag={str(self.Gmag)} Jmag={str(self.Jmag)}"
