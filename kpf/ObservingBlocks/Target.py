33c19750138e790c15a53e60255459ad524fd0e2
Josh Walawender <jmwalawender@gmail.com>
2025-12-01 19:13:54 -1000

    Use .value instead of float

--- kpf/ObservingBlocks/Target.py
+++ kpf/ObservingBlocks/Target.py
@@ -116,9 +116,9 @@ class Target(BaseOBComponent):
         else:
             out += f" {self.Equinox}"
         if abs(self.PMRA.value) > 0 or abs(self.PMDEC.value) > 0:
-               out += f" pmra={float(self.PMRA):.4f} pmdec={float(self.PMDEC):.3f}"
+               out += f" pmra={self.PMRA.value:.4f} pmdec={self.PMDEC.value:.3f}"
         if abs(self.DRA.value) > 0 or abs(self.DDEC.value) > 0:
-               out += f" dra={float(self.DRA):.3f} ddec={float(self.DDEC):.3f}"
+               out += f" dra={self.DRA.value:.3f} ddec={self.DDEC.value:.3f}"
 #         out += f" # Gmag={str(self.Gmag)} Jmag={str(self.Jmag)}"
         return out
 
